9860c700f8116271c503906559fcefd10b1fae94
BBQ_God <1827691276a@sina.com>
2024-12-13 14:19:03 +0800

    fix scripts

--- KNN_FS_LLM_code/run_exp.sh
+++ KNN_FS_LLM_code/run_exp.sh
@@ -4,11 +4,11 @@ cd KNN_FS_LLM_code
 
 # knn
 
-# python experiment_knn.py \
-#     -d bank-marketing \
-#     -t classification \
-#     -k 15 \
-#     -nthread 16 > log.bank-marketing.knn15.txt
+python experiment_knn.py \
+    -d bank-marketing \
+    -t classification \
+    -k 15 \
+    -nthread 16 > log.bank-marketing.knn15.txt
 
 python experiment_knn.py \
     -d boston-housing \
@@ -16,95 +16,95 @@ python experiment_knn.py \
     -k 15 \
     -nthread 16 > log.boston-housing.knn15.txt
 
-# python experiment_knn.py \
-#     -d breast-cancer-{} \
-#     -t classification-test \
-#     -k 3 \
-#     -nthread 16 > log.breast-cancer.knn3.txt
+python experiment_knn.py \
+    -d breast-cancer-{} \
+    -t classification-test \
+    -k 3 \
+    -nthread 16 > log.breast-cancer.knn3.txt
 
-# # one-shot learning
+# one-shot learning
 
-# python experiment.py \
-#     -d bank-marketing-wo-scaled.downsample \
-#     -t classification \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 1 \
-#     -bs 5 \
-#     -nthread 16 > log.bank-marketing.downsample.1shot.bs5.txt
+python experiment.py \
+    -d bank-marketing-wo-scaled.downsample \
+    -t classification \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 1 \
+    -bs 5 \
+    -nthread 16 > log.bank-marketing.downsample.1shot.bs5.txt
 
-# python experiment.py \
-#     -d boston-housing-wo-scaled \
-#     -t regression \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 1 \
-#     -bs 5 \
-#     -nthread 16 > log.boston-housing.1shot.bs5.txt
+python experiment.py \
+    -d boston-housing-wo-scaled \
+    -t regression \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 1 \
+    -bs 5 \
+    -nthread 16 > log.boston-housing.1shot.bs5.txt
 
-# python experiment.py \
-#     -d breast-cancer-{}-wo-scaled \
-#     -t classification-test \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 1 \
-#     -bs 1 \
-#     -nthread 16 > log.breast-cancer.1shot.bs1.txt
+python experiment.py \
+    -d breast-cancer-{}-wo-scaled \
+    -t classification-test \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 1 \
+    -bs 1 \
+    -nthread 16 > log.breast-cancer.1shot.bs1.txt
 
-# # few-shot learning
+# few-shot learning
 
-# python experiment.py \
-#     -d bank-marketing-wo-scaled.downsample \
-#     -t classification \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 3 \
-#     -bs 5 \
-#     -nthread 16 > log.bank-marketing.downsample.3shot.bs5.txt
+python experiment.py \
+    -d bank-marketing-wo-scaled.downsample \
+    -t classification \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 3 \
+    -bs 5 \
+    -nthread 16 > log.bank-marketing.downsample.3shot.bs5.txt
 
-# python experiment.py \
-#     -d boston-housing-wo-scaled \
-#     -t regression \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 3 \
-#     -bs 5 \
-#     -nthread 16 > log.boston-housing.3shot.bs5.txt
+python experiment.py \
+    -d boston-housing-wo-scaled \
+    -t regression \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 3 \
+    -bs 5 \
+    -nthread 16 > log.boston-housing.3shot.bs5.txt
 
-# python experiment.py \
-#     -d breast-cancer-{}-wo-scaled \
-#     -t classification-test \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 3 \
-#     -bs 1 \
-#     -nthread 16 > log.breast-cancer.3shot.bs1.txt
+python experiment.py \
+    -d breast-cancer-{}-wo-scaled \
+    -t classification-test \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 3 \
+    -bs 1 \
+    -nthread 16 > log.breast-cancer.3shot.bs1.txt
 
-# # many-shot learning
+# many-shot learning
 
-# python experiment.py \
-#     -d bank-marketing-wo-scaled.downsample \
-#     -t classification \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 16 \
-#     -bs 5 \
-#     -nthread 16 > log.bank-marketing.downsample.16shot.bs5.txt
+python experiment.py \
+    -d bank-marketing-wo-scaled.downsample \
+    -t classification \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 16 \
+    -bs 5 \
+    -nthread 16 > log.bank-marketing.downsample.16shot.bs5.txt
 
-# python experiment.py \
-#     -d boston-housing-wo-scaled \
-#     -t regression \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 16 \
-#     -bs 5 \
-#     -nthread 16 > log.boston-housing.16shot.bs5.txt
+python experiment.py \
+    -d boston-housing-wo-scaled \
+    -t regression \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 16 \
+    -bs 5 \
+    -nthread 16 > log.boston-housing.16shot.bs5.txt
 
-# python experiment.py \
-#     -d breast-cancer-{}-wo-scaled \
-#     -t classification-test \
-#     -ak <api_key> \
-#     -bu <base_url> \
-#     -k 16 \
-#     -bs 1 \
-#     -nthread 16 > log.breast-cancer.16shot.bs1.txt
+python experiment.py \
+    -d breast-cancer-{}-wo-scaled \
+    -t classification-test \
+    -ak <api_key> \
+    -bu <base_url> \
+    -k 16 \
+    -bs 1 \
+    -nthread 16 > log.breast-cancer.16shot.bs1.txt
--- XGBoost_code/run_exp.sh
+++ XGBoost_code/run_exp.sh
@@ -11,108 +11,108 @@ cd XGBoost_code
 #     -g 0 0.1 0.3 \
 #     -nthread 16 > log.txt
 
-# # best param
-# python experiment.py \
-#     -d bank-marketing \
-#     -t classification \
-#     -lr 0.1 \
-#     -md 7 \
-#     -g 0.3 \
-#     -nthread 16 > log.bank-marketing.txt
+# best param
+python experiment.py \
+    -d bank-marketing \
+    -t classification \
+    -lr 0.1 \
+    -md 7 \
+    -g 0.3 \
+    -nthread 16 > log.bank-marketing.txt
 
-# python experiment.py \
-#     -d boston-housing \
-#     -t regression \
-#     -lr 0.1 \
-#     -md 7 \
-#     -g 0.3 \
-#     -nthread 16 > log.boston-housing.txt
+python experiment.py \
+    -d boston-housing \
+    -t regression \
+    -lr 0.1 \
+    -md 7 \
+    -g 0.3 \
+    -nthread 16 > log.boston-housing.txt
 
-# python experiment.py \
-#     -d breast-cancer-{} \
-#     -t classification-test \
-#     -lr 0.1 \
-#     -md 7 \
-#     -g 0.3 \
-#     -nthread 16 > log.breast-cancer.txt
-
-
-# python experiment.py \
-#     -d bank-marketing.downsample \
-#     -t classification \
-#     -lr 0.1 \
-#     -md 7 \
-#     -g 0.3 \
-#     -nthread 16 > log.bank-marketing.downsample.txt
-
-# # ttt
-# CUDA_VISIBLE_DEVICES=4 \
-# python experiment_ttt.py \
-#     -d bank-marketing.downsample \
-#     -t classification \
-#     -lr 0.1 \
-#     -md 7 \
-#     -k 1000 \
-#     -g 0.3 \
-#     --device cuda \
-#     -nthread 128 > log.bank-marketing.downsample.ttt.txt
+python experiment.py \
+    -d breast-cancer-{} \
+    -t classification-test \
+    -lr 0.1 \
+    -md 7 \
+    -g 0.3 \
+    -nthread 16 > log.breast-cancer.txt
 
 
-# CUDA_VISIBLE_DEVICES=4 \
-# python experiment_ttt.py \
-#     -d breast-cancer-{} \
-#     -t classification-test \
-#     -lr 0.1 \
-#     -md 7 \
-#     -k 50 \
-#     -g 0.3 \
-#     --device cuda \
-#     -nthread 4 > log.breast-cancer.ttt50.txt
+python experiment.py \
+    -d bank-marketing.downsample \
+    -t classification \
+    -lr 0.1 \
+    -md 7 \
+    -g 0.3 \
+    -nthread 16 > log.bank-marketing.downsample.txt
 
-# CUDA_VISIBLE_DEVICES=4 \
-# python experiment_ttt.py \
-#     -d breast-cancer-{} \
-#     -t classification-test \
-#     -lr 0.1 \
-#     -md 7 \
-#     -k 25 \
-#     -g 0.3 \
-#     --device cuda \
-#     -nthread 4 > log.breast-cancer.ttt25.txt
-
-# CUDA_VISIBLE_DEVICES=4 \
-# python experiment_ttt.py \
-#     -d breast-cancer-{} \
-#     -t classification-test \
-#     -lr 0.1 \
-#     -md 7 \
-#     -k 12 \
-#     -g 0.3 \
-#     --device cuda \
-#     -nthread 4 > log.breast-cancer.ttt12.txt
-
-# CUDA_VISIBLE_DEVICES=4 \
-# python experiment_ttt.py \
-#     -d breast-cancer-{} \
-#     -t classification-test \
-#     -lr 0.1 \
-#     -md 7 \
-#     -k 3 \
-#     -g 0.3 \
-#     --device cuda \
-#     -nthread 4 > log.breast-cancer.ttt3.txt
+# ttt
+CUDA_VISIBLE_DEVICES=4 \
+python experiment_ttt.py \
+    -d bank-marketing.downsample \
+    -t classification \
+    -lr 0.1 \
+    -md 7 \
+    -k 1000 \
+    -g 0.3 \
+    --device cuda \
+    -nthread 128 > log.bank-marketing.downsample.ttt.txt
 
 
-# CUDA_VISIBLE_DEVICES=6 \
-# python experiment_ttt.py \
-#     -d boston-housing \
-#     -t regression \
-#     -lr 0.1 \
-#     -md 7 \
-#     -k 128 \
-#     -g 0.3 \
-#     --device cuda \
-#     -nthread 64 > log.boston-housing.ttt100.txt
+CUDA_VISIBLE_DEVICES=4 \
+python experiment_ttt.py \
+    -d breast-cancer-{} \
+    -t classification-test \
+    -lr 0.1 \
+    -md 7 \
+    -k 50 \
+    -g 0.3 \
+    --device cuda \
+    -nthread 4 > log.breast-cancer.ttt50.txt
+
+CUDA_VISIBLE_DEVICES=4 \
+python experiment_ttt.py \
+    -d breast-cancer-{} \
+    -t classification-test \
+    -lr 0.1 \
+    -md 7 \
+    -k 25 \
+    -g 0.3 \
+    --device cuda \
+    -nthread 4 > log.breast-cancer.ttt25.txt
+
+CUDA_VISIBLE_DEVICES=4 \
+python experiment_ttt.py \
+    -d breast-cancer-{} \
+    -t classification-test \
+    -lr 0.1 \
+    -md 7 \
+    -k 12 \
+    -g 0.3 \
+    --device cuda \
+    -nthread 4 > log.breast-cancer.ttt12.txt
+
+CUDA_VISIBLE_DEVICES=4 \
+python experiment_ttt.py \
+    -d breast-cancer-{} \
+    -t classification-test \
+    -lr 0.1 \
+    -md 7 \
+    -k 3 \
+    -g 0.3 \
+    --device cuda \
+    -nthread 4 > log.breast-cancer.ttt3.txt
+
+
+CUDA_VISIBLE_DEVICES=6 \
+python experiment_ttt.py \
+    -d boston-housing \
+    -t regression \
+    -lr 0.1 \
+    -md 7 \
+    -k 128 \
+    -g 0.3 \
+    --device cuda \
+    -nthread 64 > log.boston-housing.ttt100.txt
 
 CUDA_VISIBLE_DEVICES=2 \
 python experiment_ttt.py \
